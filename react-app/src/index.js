72f40a03f80c27f91bdd7722028c1803805bd109
NeLu <wf.nelu@yandex.ru>
2019-03-27 23:20:34 +0100

    New Component : Image

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Image from './Image';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Image />, document.getElementById('img'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
